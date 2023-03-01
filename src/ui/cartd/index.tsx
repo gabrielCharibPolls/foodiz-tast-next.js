687496da7fca6bf64c45bdf3723d4d0b73fb2a80
pyNightborn <gabriel.charib@nightborn.com>
2023-03-01 16:01:04 +0100

    adapt the display to different data length

--- src/ui/cartd/index.tsx
+++ src/ui/cartd/index.tsx
@@ -13,14 +13,20 @@ const Carte: React.FC<CarteProps> = ({ name, mail, role }) => {
       bg='white'
       padding='16px'
     >
-      <HStack spacing='24px'>
-        <Text as='b'>{name}</Text>
-        <Text size='14px' color='#8F95B2' paddingLeft='100px'>
-          {mail}
-        </Text>
-        <Text size='14px' color='#8F95B2' paddingLeft='100px'>
-          {role}
-        </Text>
+      <HStack spacing='24px' flex='1'>
+        <Flex flexDir='column' alignItems='center'>
+          <Text as='b'>{name}</Text>
+        </Flex>
+        <Flex flexDir='column' alignItems='center'>
+          <Text size='14px' color='#8F95B2'>
+            {mail}
+          </Text>
+        </Flex>
+        <Flex flexDir='column' alignItems='center'>
+          <Text size='14px' color='#8F95B2'>
+            {role}
+          </Text>
+        </Flex>
       </HStack>
       <Flex
         bg='teal.500'
