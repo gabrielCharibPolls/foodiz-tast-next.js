3fdbfca43ab6aeefc20b7378a62a4cbbdfdfb841
pyNightborn <gabrielcharib@gmail.com>
2023-03-10 11:40:08 +0100

    création composant genereique butonText

---
+++ src/ui/buttons/buttonText/props.tsx
@@ -0,0 +1,10 @@
+export interface ButtonProps {
+  text?: string;
+  styleFont?: string;
+  colorFont?: string;
+  bg?: string;
+  variant?: string;
+  width?: string;
+  height?: string;
+  colorScheme?: string;
+}
